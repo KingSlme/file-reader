aabc289c0dc146e58239ac398d7908da3c83fafc
KingSlme <djgreene100@gmail.com>
2023-12-20 23:55:49 -0500

    Add initial header files

---
+++ include/option_handler.h
@@ -0,0 +1,26 @@
+#ifndef OPTION_HANDLER_H
+#define OPTION_HANDLER_H
+
+#include <string>
+#include <map>
+
+int handleOptions(int argc, char* argv[]);
+
+void help();
+
+int handleModeOption(std::map<std::string, std::string> &optionMap, const std::string &currentFlag, const std::string &nextArgument);
+int handleVerboseOption(std::map<std::string, std::string> &optionMap, const std::string &currentFlag, const std::string &nextArgument);
+int handleOutputOption(std::map<std::string, std::string> &optionMap, const std::string &currentFlag, const std::string &nextArgument);
+
+bool checkDuplicateFlag(const std::map<std::string, std::string> &optionMap, const std::string &currentFlag);
+bool checkMissingArgument(const std::string &currentFlag, const std::string &nextArgument);
+bool checkInvalidFileName(const std::string &currentFlag, const std::string &nextArgument);
+
+void handleMissingFlag();
+void handleDuplicateFlag(const std::string &currentFlag);
+void handleUnrecognizedFlag(const std::string &currentFlag);
+void handleMissingArgument(const std::string &currentFlag);
+void handleInvalidArgument(const std::string &currentFlag, const std::string &nextArgument);
+void handleInvalidFileName(const std::string &currentFlag, const std::string &nextArgument);
+
+#endif
